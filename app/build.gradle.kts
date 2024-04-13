48647bba048d69b54784e8992cf7b83c90eaf71f
Sapoleone <154547272+Sapoleone@users.noreply.github.com>
2024-04-13 21:57:59 +0200

    Updated beta (2.0-beta_01)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sapoleone.morse"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "2.0-beta_01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
